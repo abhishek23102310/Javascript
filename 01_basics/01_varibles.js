a265b21ce38b67989efe3bd45393399f761f2c8d
abhishek23102310 <95114441+abhishek23102310@users.noreply.github.com>
2024-04-21 19:12:30 +0000

    data types

---
+++ 01_basics/01_varibles.js
@@ -0,0 +1,7 @@
+const id=1234 // we can't change const variable
+let gmail="abhishekrajput2310@gmail.com"
+var password="Hello2310" // in modern time we don't use this because of { } block scope
+let accountpass// without a value it take undefined
+console.log(id)
+accountCity='Patna' // we also declare without a varible but this is not a good parctice 
+console.table([id,gmail,password,accountCity,accountpass])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,11 @@
+"use strict";  // for new version of javascript
+// number ==> 2 to the power of 54
+// bigint
+// string ==> ""
+// boolean ==> true/false
+// undefined
+// null ==> standalone value
+// symbol ==>unique
+console.log( 3)
+//typeof
+console.log(typeof true)
